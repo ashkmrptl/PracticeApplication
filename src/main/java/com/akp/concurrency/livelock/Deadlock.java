b73d5bf5d2fd983b69c9262901909a321554848a
ashish.patel <ashish.patel@impelsys.com>
2020-08-19 10:07:53 +0530

    Update Deadlock Tester

--- src/main/java/com/akp/concurrency/livelock/Deadlock.java
+++ src/main/java/com/akp/concurrency/livelock/Deadlock.java
@@ -1,32 +1,32 @@
 package com.akp.concurrency.livelock;
 
 public class Deadlock {
-	private Object object1 = new Object();
-	private Object object2 = new Object();
+    private final Object object1 = new Object();
+    private final Object object2 = new Object();
 
-	public void methodOne() {
-		synchronized (object1) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			synchronized (object2) {
-				System.out.println("Method one");
-			}
-		}
-	}
+    public void methodOne() {
+        synchronized (object1) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (object2) {
+                System.out.println("Method one");
+            }
+        }
+    }
 
-	public void methodTwo() {
-		synchronized (object2) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			synchronized (object1) {
-				System.out.println("Method two");
-			}
-		}
-	}
+    public void methodTwo() {
+        synchronized (object2) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (object1) {
+                System.out.println("Method two");
+            }
+        }
+    }
 }
